46568ae3169cfbd0f24601f6e1a9e28c6dd49611
Lochana.synotec <lochana.synotec@gmail.com>
2022-12-29 08:59:46 +0530

    #60 Profile page design - Backend

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -10,46 +10,77 @@
     @endsection
 
     <div class="row">
-        <div class="col-sm-12">
-            <div class="card">
-                <div class="card-body">
-                    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-                        @if (Laravel\Fortify\Features::canUpdateProfileInformation())
-                            @livewire('profile.update-profile-information-form')
 
-                            <x-jet-section-border/>
-                        @endif
-
-                        @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
-                            <div class="mt-10 sm:mt-0">
-                                @livewire('profile.update-password-form')
+        <div class="col-xl-3 col-lg-4">
+            <div class="clearfix">
+                <div class="card card-bx profile-card author-profile m-b30">
+                    <div class="card-body">
+                        <div class="p-5">
+                            <div class="author-profile">
+                                <div class="author-media">
+                                    <img src="{{ auth()->user()->profile_photo_url }}" alt="" class="profile-image2">
+                                    <div class="upload-link" title="" data-toggle="tooltip" data-placement="right"
+                                        data-original-title="update">
+                                        <input type="file" class="update-flie">
+                                        <i class="fa fa-camera"></i>
+                                    </div>
+                                </div>
+                                <div class="author-info">
+                                    <h6 class="title">{{ auth()->user()->name }}</h6>
+                                    <span>{{ auth()->user()->username }}</span>
+                                </div>
                             </div>
-
-                            <x-jet-section-border/>
-                        @endif
-
-                        @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
-                            <div class="mt-10 sm:mt-0">
-                                @livewire('profile.two-factor-authentication-form')
-                            </div>
-
-                            <x-jet-section-border/>
-                        @endif
-
-                        <div class="mt-10 sm:mt-0">
-                            @livewire('profile.logout-other-browser-sessions-form')
                         </div>
-
-                        @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
-                            <x-jet-section-border/>
-
-                            <div class="mt-10 sm:mt-0">
-                                @livewire('profile.delete-user-form')
-                            </div>
-                        @endif
+                        <div class="info-list">
+                            <ul>
+                                <li><a href="app-profile.html">Models</a><span>36</span></li>
+                                <li><a href="uc-lightgallery.html">Gallery</a><span>3</span></li>
+                                <li><a href="app-profile.html">Lessons</a><span>1</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="input-group mb-3">
+                            <div class="form-control rounded text-center bg-white">Portfolio</div>
+                        </div>
+                        <div class="input-group">
+                            <a href="https://www.dexignzone.com/"
+                                class="form-control text-primary rounded text-start bg-white">https://www.dexignzone.com/</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+
+        <div class="col-xl-9 col-lg-8">
+
+            @if (Laravel\Fortify\Features::canUpdateProfileInformation())
+                @livewire('profile.update-profile-information-form')
+            @endif
+
+            @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
+                <div class="mt-10 sm:mt-0">
+                    @livewire('profile.update-password-form')
+                </div>
+            @endif
+
+            @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
+                <div class="mt-10 sm:mt-0">
+                    @livewire('profile.two-factor-authentication-form')
+                </div>
+            @endif
+
+            <div class="mt-10 sm:mt-0">
+                @livewire('profile.logout-other-browser-sessions-form')
+            </div>
+
+            @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
+                <div class="mt-10 sm:mt-0">
+                    @livewire('profile.delete-user-form')
+                </div>
+            @endif
+
+        </div>
     </div>
+
 </x-backend.layouts.app>
