587df80f9de861ef095b96f60d9be16ff8bc918f
Lochana.synotec <lochana.synotec@gmail.com>
2023-01-01 17:44:19 +0530

    #76 Genealogy card design in user panel

---
+++ public/assets/backend/js/user/genealogy/owl.js
@@ -0,0 +1,15 @@
+
+
+ alert('sss');
+$(function() {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+
+    owl.owlCarousel({
+        items: 3,
+        margin: 10,
+        loop: true,
+        nav: true
+    });
+
+});
