661dfb3bf7406764bcb86f49d7ef1f15fa6e4c41
hansajith18 <hansajith18@gmail.com>
2023-01-07 20:20:52 +0530

    #100 Currency Image

--- public/assets/backend/js/admin/cms/currency.js
+++ public/assets/backend/js/admin/cms/currency.js
@@ -24,12 +24,39 @@ $(function () {
     // document.getElementById('currency-form').addEventListener('submit', saveCurrency)
 
     function saveCurrency(e) {
+
         e.preventDefault();
+
         loader()
-        $('#currency-form').find(".text-danger").remove();
-        let formData = $('#currency-form').serialize();
-        axios.post(`${APP_URL}/admin/currencies`, formData)
-            .then(response => {
+        let __el = $('#currency-form');
+        __el.find(".text-danger").remove();
+
+        if ($("#name").val().length <= 0) {
+            Toast.fire({
+                icon: 'error', title: "Name is required",
+            })
+        } else if ($("#value").val().length <= 0) {
+            Toast.fire({
+                icon: 'error', title: "Value is required",
+            })
+        } else if ($("#change").val().length <= 0) {
+            Toast.fire({
+                icon: 'error', title: "Change is required",
+            })
+        } else if ($("#image_data").val().length <= 0) {
+            Toast.fire({
+                icon: 'error', title: "Image is required",
+            })
+        } else {
+
+            let image_name = $('#image_data').val();
+            let name = $('#name').val()
+            let value = $('#value').val()
+            let change = $('#change').val()
+
+            axios.post(`${APP_URL}/admin/currencies`, {
+                image_name, name, value, change
+            }).then(response => {
                 Toast.fire({
                     icon: response.data.icon, title: response.data.message,
                 }).then(res => {
@@ -38,38 +65,46 @@ $(function () {
                     }
                 })
             }).catch((error) => {
-            Toast.fire({
-                icon: 'error', title: error.response.data.message || "Something went wrong!",
+                Toast.fire({
+                    icon: 'error', title: error.response.data.message || "Something went wrong!",
+                })
+                console.error(error.response.data)
+                let errorMap = ['name', 'value', 'change', 'image_name']
+                errorMap.map(id => {
+                    error.response.data.errors[id] && appendError(id, `<span class="text-danger">${error.response.data.errors[id]}</span>`)
+                })
             })
-            console.error(error.response.data)
-            let errorMap = ['name', 'iso']
-            errorMap.map(id => {
-                error.response.data.errors[id] && appendError(id, `<span class="text-danger">${error.response.data.errors[id]}</span>`)
-            })
-        })
 
+        }
     }
 
     function updateCurrency(e) {
         e.preventDefault();
         loader()
-        $('#currency-form').find(".text-danger").remove();
-        let formData = $('#currency-form').serialize();
-        axios.patch(`${APP_URL}/admin/currencies/${CURRENCY_ID}`, formData)
-            .then(response => {
-                Toast.fire({
-                    icon: response.data.icon, title: response.data.message,
-                }).then(res => {
-                    if (response.data.status) {
-                        location.reload();
-                    }
-                })
-            }).catch((error) => {
+        let __el = $('#currency-form');
+        __el.find(".text-danger").remove();
+
+        let image_name = $('#image_data').val();
+        let name = $('#name').val()
+        let value = $('#value').val()
+        let change = $('#change').val()
+
+        axios.patch(`${APP_URL}/admin/currencies/${CURRENCY_ID}`, {
+            image_name, name, value, change
+        }).then(response => {
+            Toast.fire({
+                icon: response.data.icon, title: response.data.message,
+            }).then(res => {
+                if (response.data.status) {
+                    location.reload();
+                }
+            })
+        }).catch((error) => {
             Toast.fire({
                 icon: 'error', title: error.response.data.message || "Something went wrong!",
             })
             console.error(error.response.data)
-            let errorMap = ['name', 'iso']
+            let errorMap = ['name', 'value', 'change', 'image_name']
             errorMap.map(id => {
                 error.response.data.errors[id] && appendError(id, `<span class="text-danger">${error.response.data.errors[id]}</span>`)
             })
@@ -84,7 +119,7 @@ $(function () {
 
     $(document).on("click", ".delete-currency", function (e) {
         e.preventDefault();
-        __this = $(this);
+        let __this = $(this);
         let currency_id = $(this).data("currency");
         Swal.fire({
             title: "Are You Sure?",
@@ -94,21 +129,35 @@ $(function () {
         }).then((willDelete) => {
             if (willDelete.isConfirmed) {
                 loader()
-                axios.delete(`${APP_URL}/admin/currencies/${currency_id}`)
-                    .then(response => {
-                        Toast.fire({
-                            icon: response.data.icon, title: response.data.message,
-                        }).then(res => {
-                            if (response.data.status) {
-                                location.reload();
-                            }
-                        })
-                    }).catch((error) => {
+                axios.delete(`${APP_URL}/admin/currencies/${currency_id}`).then(response => {
+                    Toast.fire({
+                        icon: response.data.icon, title: response.data.message,
+                    }).then(res => {
+                        if (response.data.status) {
+                            location.reload();
+                        }
+                    })
+                }).catch((error) => {
                     Toast.fire({
                         icon: 'error', title: error.response.data.message || "Something went wrong!",
                     })
                     console.error(error.response.data)
                 })
+            }
+        });
+    });
+
+
+    $("#image_name").change(function () {
+        let __this = $(this)
+        canvasResize(this.files[0], {
+            width: 600, height: 600, crop: false, quality: 80, //rotate: 90,
+            callback: function (data, width, height) {
+                __this.val(null)
+                $('#image_data').val(data)
+                $('.preview-image').remove()
+                let htmlPreview = '<img class="preview-image img-thumbnail mt-2 preview-image w-25" alt="preview-image" src="' + data + '" />';
+                $(htmlPreview).insertAfter(__this)
             }
         });
     });
--- resources/views/backend/admin/currencies/save.blade.php
+++ resources/views/backend/admin/currencies/save.blade.php
@@ -5,24 +5,32 @@
             <div class="form-group row mb-2">
                 <label class="col-sm-3 col-form-label" for="name">Currency</label>
                 <div class="col-sm-9">
-                    <input class="form-control" id="name" name="name" placeholder="Currency" type="text"
-                        value="{{ $currency->name ?? null }}">
+                    <input class="form-control" id="name" name="name" placeholder="Currency" type="text" value="{{ $currency->name ?? null }}">
                 </div>
             </div>
 
             <div class="form-group row mb-2">
                 <label class="col-sm-3 col-form-label" for="value">Value ($)</label>
                 <div class="col-sm-9">
-                    <input class="form-control" id="value" name="value" placeholder="Value" type="text"
-                        value="{{ $currency->value ?? null }}">
+                    <input class="form-control" id="value" name="value" placeholder="Value" type="text" value="{{ $currency->value ?? null }}">
                 </div>
             </div>
 
             <div class="form-group row mb-2">
                 <label class="col-sm-3 col-form-label" for="change">Change (%)</label>
                 <div class="col-sm-9">
-                    <input class="form-control" id="change" name="change" placeholder="Change (%) in Last 30 Days"
-                        type="text" value="{{ $currency->change ?? null }}">
+                    <input class="form-control" id="change" name="change" placeholder="Change (%) in Last 30 Days" type="text" value="{{ $currency->change ?? null }}">
+                </div>
+            </div>
+
+            <div class="form-group row mb-2">
+                <label class="col-sm-3 col-form-label" for="image">Image</label>
+                <div class="col-sm-9">
+                    <input class="form-control" id="image_name" type="file" accept="image/*" placeholder="Select Image">
+                    <input class="form-control" id="image_data" type="hidden">
+                    @if(!empty($currency->image_name))
+                        <img class="preview-image img-thumbnail mt-2 preview-image w-25" alt="preview-image" src="{{ storage('currencies/'.$currency->image_name) }}"/>
+                    @endif
                 </div>
             </div>
 
