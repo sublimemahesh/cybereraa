c6c58fc2b19248ee1184e45e1de3a10ae4a46de4
hansajith18 <hansajith18@gmail.com>
2023-01-08 10:41:12 +0530

    (fix:) 2FA login Alpinejs functionality

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 @extends('auth.layouts.auth')
-@section('title', 'LOGIN')
+@section('title', 'Login')
 @section('contents')
 
     <div class="row justify-content-center main-register-form-style">
@@ -9,8 +9,10 @@
                     <div class="col-xl-12">
                         <div class="auth-form">
                             <div class="text-center mb-3">
-                                <a href="{{ route('/') }}"><img src="{{ asset('assets/backend/images/logo/logo-full.png') }}"
-                                        alt=""></a>
+                                <a href="{{ route('/') }}">
+                                    <img src="{{ asset('assets/backend/images/logo/logo-full.png') }}"
+                                            alt="">
+                                </a>
                             </div>
                             <h4 class="text-center mb-4">Login to Your Account</h4>
 
@@ -19,26 +21,32 @@
                                     {{ session('status') }}
                                 </div>
                             @endif
+
+                            <x-jet-validation-errors class="mb-4"/>
+
                             <form method="POST" action="{{ route('login') }}">
                                 @csrf
 
                                 <div class="col-lg-12  mt-4">
-                                    <label class="mb-1" for="email"><strong class="main-register-form-text">{{ __('Email') }}<sup
-                                                class="main-required">*</sup></strong></label>
-                                    <x-jet-input id="email" class="block mt-1 w-full form-control" type="email"
-                                        name="email" :value="old('email')" required autofocus />
+                                    <label class="mb-1" for="email">
+                                        <strong class="main-register-form-text">{{ __('Email') }}
+                                            <sup class="main-required">*</sup>
+                                        </strong>
+                                    </label>
+                                    <x-jet-input id="email" class="block mt-1 w-full form-control" type="email" name="email" :value="old('email')" required autofocus/>
                                 </div>
 
                                 <div class="col-lg-12  mt-4">
-                                    <label class="mb-1" for="password"><strong class="main-register-form-text">{{ __('Password') }}<sup
-                                                class="main-required">*</sup></strong></label>
+                                    <label class="mb-1" for="password"><strong class="main-register-form-text">{{ __('Password') }}
+                                            <sup
+                                                    class="main-required">*</sup></strong></label>
                                     <x-jet-input id="password" class="block mt-1 w-full form-control" type="password"
-                                        name="password" required autocomplete="current-password" />
+                                            name="password" required autocomplete="current-password"/>
                                 </div>
 
                                 <div class="col-lg-12  mt-4">
                                     <label for="remember_me" class="flex items-center">
-                                        <x-jet-checkbox id="remember_me" name="remember" />
+                                        <x-jet-checkbox id="remember_me" name="remember"/>
                                         <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                                     </label>
                                 </div>
@@ -46,22 +54,22 @@
                                 <div class="col-lg-12  mt-4">
                                     <button type="submit" class="btn btn-primary btn-block">{{ __('Log in') }}</button>
                                 </div>
-                                    <div class="col-lg-12  mt-4">
-                                        <div class="flex  mt-4">
-                                            @if (Route::has('password.request'))
-                                                <a class="underline text-sm text-gray-600 hover:text-gray-900"
+                                <div class="col-lg-12  mt-4">
+                                    <div class="flex  mt-4">
+                                        @if (Route::has('password.request'))
+                                            <a class="underline text-sm text-gray-600 hover:text-gray-900"
                                                     href="{{ route('password.request') }}">
-                                                    {{ __('Forgot your password?') }}
-                                                </a>
-                                            @endif
-                                        </div>
+                                                {{ __('Forgot your password?') }}
+                                            </a>
+                                        @endif
                                     </div>
-                                    <div class="col-lg-12  mt-4">
-                                        <a class="underline text-sm text-gray-600 hover:text-gray-900"
+                                </div>
+                                <div class="col-lg-12  mt-4">
+                                    <a class="underline text-sm text-gray-600 hover:text-gray-900"
                                             href="{{ route('register') }}">
-                                            {{ __('Create Your Account ?') }}
-                                        </a>
-                                    </div>
+                                        {{ __('Create Your Account ?') }}
+                                    </a>
+                                </div>
                             </form>
 
                             <div class="new-account mt-3">
