79ace59965a1508e296cbf15be6752b5ccb00679
hansajith18 <hansajith18@gmail.com>
2023-04-06 15:18:31 +0530

    log password change activity from users

--- packages/laravel-user-activity/src/Traits/Loggable.php
+++ packages/laravel-user-activity/src/Traits/Loggable.php
@@ -2,6 +2,11 @@
 
 namespace Haruncpi\LaravelUserActivity\Traits;
 
+use App\Models\User;
+use Arr;
+use Exception;
+use Log as Logger;
+
 trait Loggable
 {
     use Log;
@@ -10,6 +15,21 @@ trait Loggable
     {
         if (config('user-activity.log_events.on_edit', false)) {
             self::updated(function ($model) {
+                try {
+                    if (($model instanceof User) && $model->isDirty('password')) {
+                        $log_data = [
+                            'RESPONSIBLE USER' => auth()->user()->only(['id', 'username', 'email']),
+                            'PREVIOUS DATA' => Arr::except($model->getRawOriginal(), $model->exclude),
+                            'NEW DATA' => $model->getDirty(),
+                            'ACTION' => 'users.change-password',
+                        ];
+                        $log_data = json_encode($log_data, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT);
+                        Logger::channel('daily')->notice('PASSWORD CHANGE ACTIVITY | DATA: ' . $log_data);
+                    }
+                } catch (Exception $e) {
+
+                }
+
                 self::logToDb($model, 'edit');
             });
         }
