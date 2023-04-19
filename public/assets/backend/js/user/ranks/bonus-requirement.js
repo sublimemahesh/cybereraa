5d2fe4b2cbbdb90d98457d10c8f7e85f91be9e75
hansajith18 <hansajith18@gmail.com>
2023-04-19 16:03:52 +0530

    #390 Bonus Requirement month filter

---
+++ public/assets/backend/js/user/ranks/bonus-requirement.js
@@ -0,0 +1,26 @@
+$(function () {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const month = urlParams.get("month");
+
+    flatpickr("#month", {
+        mode: "single",
+        minDate: "2023-03",
+        dateFormat: "Y-m",
+        defaultDate: month,
+        plugins: [
+            new monthSelectPlugin({
+                shorthand: true, //defaults to false
+                dateFormat: "Y-m", //defaults to "F Y"
+                altFormat: "F Y", //defaults to "F Y"
+                theme: "dark" // defaults to "light"
+            })
+        ]
+    });
+
+    $(document).on("click", "#search", function (e) {
+        e.preventDefault();
+        urlParams.set("month", $("#month").val());
+        location.href = location.href.split(/\?|\#/)[0] + "?" + urlParams.toString()
+    });
+})
