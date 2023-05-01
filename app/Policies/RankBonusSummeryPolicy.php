94b626875071119aa263da0dad1044faefde68dc
hansajith18 <hansajith18@gmail.com>
2023-05-01 10:46:14 +0530

    Rank bonus requirement for all users

--- app/Policies/RankBonusSummeryPolicy.php
+++ app/Policies/RankBonusSummeryPolicy.php
@@ -11,6 +11,16 @@ class RankBonusSummeryPolicy
 
     public function viewAny(User $user)
     {
+        return true;
+    }
+
+    public function viewSummery(User $user)
+    {
         return $user->currentRank->rank >= 3;
+    }
+
+    public function viewRequirement(User $user)
+    {
+        return true;
     }
 }
