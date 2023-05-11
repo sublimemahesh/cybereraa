e50754a4ec5ffaa3499fb8890b97de45c2a4e70e
hansajith18 <hansajith18@gmail.com>
2023-05-11 15:25:45 +0530

    #481 KYC & User dashboard popup msges

--- resources/views/invoices/terms.blade.php
+++ resources/views/invoices/terms.blade.php
@@ -6,12 +6,11 @@
             <li>If ur investment is cancelled by our company. the company is bound to release your investment.</li>
             <li>Invested money back guarantee</li>
         @else
-            <li>Invested amount is not refunded at once.</li>
-            <li>The benefit related to investment will be given after 3 working days.</li>
-            <li>The benefit related to investment will be given only working days.</li>
-            <li>The benefits related to the invested amount will be given within 300 working days</li>
-            <li>If ur investment is cancelled by our company. the company is bound to release your investment.</li>
-            <li>Invested money back guarantee</li>
+            <li>Invested amount is not refunded.</li>
+            <li>The benefits related to the investment will be given after 3 working days.</li>
+            <li>Package becomes active 3 working days after the top-up date.</li>
+            <li>The benefits related to the investment will be given within 300 working days.</li>
+            <li>If your investment is canceled by the company, the company is bound to release your initial investment.</li>
         @endif
     </ul>
 </div>
