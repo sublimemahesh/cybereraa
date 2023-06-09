ad62516398ebb9994c00eef220dfee936295e520
Lochana.synotec <lochana.synotec@gmail.com>
2023-06-09 15:45:17 +0530

    #10 Change the website template -Add frontend style.blade page and include app.blade page

---
+++ resources/views/frontend/layouts/style.blade.php
@@ -0,0 +1,34 @@
+ <!-- BOOTSTRAP STYLE SHEET -->
+ <link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/bootstrap.min.css') }}">
+ <!-- FONTAWESOME STYLE SHEET -->
+ <link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/fontawesome/css/font-awesome.min.css') }}" />
+ <!-- FLATICON STYLE SHEET -->
+ <link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/flaticon.min.css') }}">
+ <!-- ANIMATE STYLE SHEET -->
+ <link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/animate.min.css') }}">
+ <!-- OWL CAROUSEL STYLE SHEET -->
+ <link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/owl.carousel.min.css') }}">
+ <!-- BOOTSTRAP SELECT BOX STYLE SHEET -->
+ <link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/bootstrap-select.min.css')}}">
+ <!-- MAGNIFIC POPUP STYLE SHEET -->
+ <link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/magnific-popup.min.css')}}">
+ <!-- LOADER STYLE SHEET -->
+ <link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/loader.min.css')}}">
+ <!-- MAIN STYLE SHEET -->
+ <link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/style.css')}}">
+ <!-- THEME COLOR CHANGE STYLE SHEET -->
+ <link rel="stylesheet" class="skin" type="text/css" href="{{asset('assets/frontend/css/skin/skin-1.css')}}">
+ <!-- CUSTOM  STYLE SHEET -->
+ <link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/custom.css')}}">
+ <!-- SIDE SWITCHER STYLE SHEET -->
+ <link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/css/switcher.css')}}">
+ <!-- REVOLUTION SLIDER CSS -->
+ <link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/plugins/revolution/revolution/css/settings.css')}}">
+ <!-- REVOLUTION NAVIGATION STYLE -->
+ <link rel="stylesheet" type="text/css" href="{{asset('assets/frontend/plugins/revolution/revolution/css/navigation.css')}}">
+
+  <!-- GOOGLE FONTS -->
+  <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,300italic,400italic,500,500italic,700,700italic,900italic,900' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,800italic,800,700italic' rel='stylesheet' type='text/css'>
+  <link href="https://fonts.googleapis.com/css?family=Crete+Round:400,400i&amp;subset=latin-ext" rel="stylesheet">
+
