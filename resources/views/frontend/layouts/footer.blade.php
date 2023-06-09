fd75f3064df97cc9a6d162ecc27559d831677fe5
Lochana.synotec <lochana.synotec@gmail.com>
2023-06-09 14:13:37 +0530

    #3 Change the website template - change frontend footer.blade page

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,115 +1,198 @@
-<!-- Call To Action Section Starts -->
-<section class="call-action-all">
-    <div class="call-action-all-overlay">
+<footer class="site-footer footer-dark bg-no-repeat bg-full-height bg-center "  style="background-image:url(images/background/footer-bg.jpg);">
+    <!-- FOOTER BLOCKES START -->
+    <div class="footer-top overlay-wraper">
+        <div class="overlay-main bg-black opacity-05"></div>
         <div class="container">
             <div class="row">
-                <div class="col-xs-12">
-                    <!-- Call To Action Text Starts -->
-                    <div class="action-text">
-                        <h2>COME AND JOIN US TO WIN YOUR LIFE!...</h2>
-                        <p class="lead">What if your dream investment can be made in the safest spot on the earth? just invest and wait and enjoy up to a guaranteed return of 400% in 15 months.
-                            Daily withdrawals, No claim Bonuses, and many more massive benefits.</p>
+                <!-- ABOUT COMPANY -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="widget widget_about">
+                        <h4 class="widget-title text-white">About Company</h4>
+                        <div class="logo-footer clearfix p-b15">
+                            <a href="index-2.html"><img src="images/footer-logo.png" width="230" height="67" alt=""/></a>
+                        </div>
+                        <p>Thewebmax ipsum dolor sit amet, consectetuer adipiscing elit,
+                              sed diam nonummy nibh euismod tincidunt ut laoreet dolore agna aliquam erat .
+                              wisi enim ad minim veniam, quis tation. sit amet, consec tetuer.
+                              ipsum dolor sit amet, consectetuer adipiscing.ipsum dolor sit .
+                        </p>
                     </div>
-                    <!-- Call To Action Text Ends -->
-                    <!-- Call To Action Button Starts -->
-                    <p class="action-btn">
-                        <a class="btn btn-primary" href="{{ route('register') }}">Register Now</a>
-                    </p>
-                    <!-- Call To Action Button Ends -->
                 </div>
-            </div>
-            <div class="header-logo-img">
-                <img class='shimmer' src="{{ asset('assets/frontend/images/project/header_icon_img.png') }}"  alt="safest trades">
-            </div>
-        </div>
-    </div>
-</section>
-
-<!-- Call To Action Section Ends -->
-
-
-<!-- Footer Starts -->
-<footer class="footer">
-    <!-- Footer Top Area Starts -->
-    <div class="top-footer">
-        <div class="container">
-            <div class="row">
-                <!-- Footer Widget Starts -->
-                <div class="col-sm-4 col-md-3">
-                    <h4>Short Links</h4>
-                    <div class="menu">
+                <!-- RESENT POST -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="widget recent-posts-entry-date">
+                        <h4 class="widget-title text-white">Resent Post</h4>
+                        <div class="widget-post-bx">
+                            <div class="bdr-light-blue widget-post clearfix  bdr-b-1 m-b10 p-b10">
+                                <div class="wt-post-date text-center text-uppercase text-white p-t5">
+                                    <strong>20</strong>
+                                    <span>Mar</span>
+                                </div>
+                                <div class="wt-post-info">
+                                    <div class="wt-post-header">
+                                        <h6 class="post-title"><a href="blog-single.html">Blog title first </a></h6>
+                                    </div>
+                                    <div class="wt-post-meta">
+                                        <ul>
+                                            <li class="post-author"><i class="fa fa-user"></i>By Admin</li>
+                                            <li class="post-comment"><i class="fa fa-comments"></i> 28</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="bdr-light-blue widget-post clearfix  bdr-b-1 m-b10 p-b10">
+                                <div class="wt-post-date text-center text-uppercase text-white p-t5">
+                                    <strong>30</strong>
+                                    <span>Mar</span>
+                                </div>
+                                <div class="wt-post-info">
+                                    <div class="wt-post-header">
+                                        <h6 class="post-title"><a href="blog-single.html">Blog title first </a></h6>
+                                    </div>
+                                    <div class="wt-post-meta">
+                                        <ul>
+                                            <li class="post-author"><i class="fa fa-user"></i>By Admin</li>
+                                            <li class="post-comment"><i class="fa fa-comments"></i> 29</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="bdr-light-blue widget-post clearfix  bdr-b-1 m-b10 p-b10">
+                                <div class="wt-post-date text-center text-uppercase text-white p-t5">
+                                    <strong>31</strong>
+                                    <span>Mar</span>
+                                </div>
+                                <div class="wt-post-info">
+                                    <div class="wt-post-header">
+                                        <h6 class="post-title"><a href="blog-single.html">Blog title first </a></h6>
+                                    </div>
+                                    <div class="wt-post-meta">
+                                        <ul>
+                                            <li class="post-author"><i class="fa fa-user"></i>By Admin</li>
+                                            <li class="post-comment"><i class="fa fa-comments"></i> 30</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- USEFUL LINKS -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="widget widget_services">
+                        <h4 class="widget-title text-white">Useful links</h4>
                         <ul>
-                            <li>
-                                <a href="{{ route('/') }}">Home</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('about') }}">About Us</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('contact') }}">Contact</a>
-                            </li>
+                            <li><a href="about-1.html">About</a></li>
+                            <li><a href="faq-1.html">FAQ</a></li>
+                            <li><a href="our-team.html">Our Team</a></li>
+                            <li><a href="services-1.html">Services</a></li>
+                            <li><a href="contact-1.html">Contact Us</a></li>
                         </ul>
                     </div>
                 </div>
-                <!-- Footer Widget Ends -->
-                <!-- Footer Widget Starts -->
-                <div class="col-sm-4 col-md-3">
-                    <h4>Important Links</h4>
-                    <div class="menu">
-                        <ul>
-                            <li>
-                                <a href="{{ route('faq') }}">FAQ</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('terms&Conditions') }}">Terms and conditions</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('disclaimer') }}">Disclaimer</a>
-                            </li>
+                <!-- NEWSLETTER -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="widget widget_newsletter">
+                        <h4 class="widget-title text-white">Newsletter</h4>
+                        <div class="newsletter-bx">
+                            <form role="search" method="post">
+                                <div class="input-group">
+                                <input name="news-letter" class="form-control" placeholder="ENTER YOUR EMAIL" type="text">
+                                <span class="input-group-btn">
+                                    <button type="submit" class="site-button"><i class="fa fa-paper-plane-o"></i></button>
+                                </span>
+                            </div>
+                             </form>
+                        </div>
+                    </div>
+                    <!-- SOCIAL LINKS -->
+                    <div class="widget widget_social_inks">
+                        <h4 class="widget-title text-white">Social Links</h4>
+                        <ul class="social-icons social-square social-darkest">
+                            <li><a href="javascript:void(0);" class="fa fa-facebook"></a></li>
+                            <li><a href="javascript:void(0);" class="fa fa-twitter"></a></li>
+                            <li><a href="javascript:void(0);" class="fa fa-linkedin"></a></li>
+                            <li><a href="javascript:void(0);" class="fa fa-rss"></a></li>
+                            <li><a href="javascript:void(0);" class="fa fa-youtube"></a></li>
+                            <li><a href="javascript:void(0);" class="fa fa-instagram"></a></li>
                         </ul>
                     </div>
                 </div>
-                <!-- Footer Widget Ends -->
-                <!-- Footer Widget Starts -->
-                <div class="col-sm-4 col-md-3">
-                    <h4>Contact Us </h4>
-                    <div class="contacts">
-                        <div>
-                            <span> <a href="mailto:support@safesttrades.com">support@safesttrades.com</a></span>
+            </div>
+            <div class="row">
+
+               <div class="col-md-3 col-sm-6  p-tb20">
+                   <div class="wt-icon-box-wraper left  bdr-1 bdr-gray-dark p-tb15 p-lr10 clearfix">
+                        <div class="icon-md text-primary">
+                            <span class="iconmoon-travel"></span>
                         </div>
-                        <div>
-                            <span> Safest Trades, Proton Trading Pro LLC., 140 21ST ST STE R, Sacramento, CA 95811.</span>
+                        <div class="icon-content text-white">
+                            <h5 class="wt-tilte text-uppercase m-b0">Address</h5>
+                            <p>No.123 Chalingt Gates, Supper market New York</p>
+                        </div>
+                   </div>
+                </div>
+               <div class="col-md-3 col-sm-6  p-tb20 ">
+                   <div class="wt-icon-box-wraper left  bdr-1 bdr-gray-dark p-tb15 p-lr10 clearfix ">
+                        <div class="icon-md text-primary">
+                            <span class="iconmoon-smartphone-1"></span>
+                        </div>
+                        <div class="icon-content text-white">
+                            <h5 class="wt-tilte text-uppercase m-b0">Phone</h5>
+                            <p class="m-b0">+41 555 888 9585</p>
+                            <p>+41 555 888 9585</p>
+                        </div>
+                   </div>
+               </div>
+               <div class="col-md-3 col-sm-6  p-tb20">
+                   <div class="wt-icon-box-wraper left  bdr-1 bdr-gray-dark p-tb15 p-lr10 clearfix">
+                        <div class="icon-md text-primary">
+                            <span class="iconmoon-fax"></span>
+                        </div>
+                        <div class="icon-content text-white">
+                            <h5 class="wt-tilte text-uppercase m-b0">Fax</h5>
+                            <p class="m-b0">FAX: (123) 123-4567</p>
+                            <p>FAX: (123) 123-4567</p>
                         </div>
                     </div>
-                    <!-- Social Media Profiles Ends -->
                 </div>
-                <!-- Footer Widget Ends -->
-                <!-- Footer Widget Starts -->
-                <div class="col-sm-12 col-md-3">
-                    {!! $footer_numbers['social_media_links']->content !!}
+               <div class="col-md-3 col-sm-6 p-tb20">
+                   <div class="wt-icon-box-wraper left  bdr-1 bdr-gray-dark p-tb15 p-lr10 clearfix">
+                        <div class="icon-md text-primary">
+                            <span class="iconmoon-email"></span>
+                        </div>
+                        <div class="icon-content text-white">
+                            <h5 class="wt-tilte text-uppercase m-b0">Email</h5>
+                            <p class="m-b0">info@demo.com</p>
+                            <p>info@demo1234.com</p>
+                        </div>
+                    </div>
                 </div>
-                <!-- Footer Widget Ends -->
-            </div>
+
+          </div>
         </div>
     </div>
-    <!-- Footer Top Area Ends -->
-    <!-- Footer Bottom Area Starts -->
-    <div class="bottom-footer">
-        <div class="container">
+    <!-- FOOTER COPYRIGHT -->
+    <div class="footer-bottom  overlay-wraper">
+        <div class="overlay-main"></div>
+        <div class="constrot-strip"></div>
+        <div class="container p-t30">
             <div class="row">
-                <div class="col-xs-12">
-                    <!-- Copyright Text Starts -->
-                    <p class="text-center">
-                        © <?php echo date("Y"); ?> Safest Trades. All Rights Reserved.
-                    </p>
-                    <!-- Copyright Text Ends -->
+                <div class="wt-footer-bot-left">
+                    <span class="copyrights-text">© 2023 . All Rights Reserved. Designed By Synotec Holdings Pvt. Ltd.</span>
+                </div>
+                <div class="wt-footer-bot-right">
+                    <ul class="copyrights-nav pull-right">
+                        <li><a href="javascript:void(0);">Terms  & Condition</a></li>
+                        <li><a href="javascript:void(0);">Privacy Policy</a></li>
+                        <li><a href="contact-1.html">Contact Us</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Footer Bottom Area Ends -->
 </footer>
-<!-- Footer Ends -->
-<!-- Back To Top Starts  -->
-<a href="#" id="back-to-top" class="back-to-top fa fa-arrow-up"></a>
-<!-- Back To Top Ends  -->
+
+<!-- BUTTON TOP START -->
+<button class="scroltop"><span class=" iconmoon-house relative" id="btn-vibrate"></span>Top</button>
+<!-- BUTTON TOP END -->
