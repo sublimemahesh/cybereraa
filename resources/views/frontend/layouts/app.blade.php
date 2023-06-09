857c5076c06cc1ce5b6153afc9667cca9d6bddef
Lochana.synotec <lochana.synotec@gmail.com>
2023-06-09 15:36:45 +0530

    #8 Change the website template -Add frontend script.blade page and include app.blade page

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -56,35 +56,10 @@
     {{ $slot }}
 
     @include('frontend.layouts.footer')
+    
     <!-- Template JS Files -->
 
-    <!-- JAVASCRIPT  FILES ========================================= -->
-    <script src="{{ asset('assets/frontend/js/jquery-1.12.4.min.js') }}"></script><!-- JQUERY.MIN JS -->
-    <script src="{{ asset('assets/frontend/js/bootstrap.min.js') }}"></script><!-- BOOTSTRAP.MIN JS -->
-
-    <script src="{{ asset('assets/frontend/js/bootstrap-select.min.js') }}"></script><!-- FORM JS -->
-    <script src="{{ asset('assets/frontend/js/jquery.bootstrap-touchspin.min.js') }}"></script><!-- FORM JS -->
-
-
-    <script src="{{ asset('assets/frontend/js/magnific-popup.min.js') }}"></script><!-- MAGNIFIC-POPUP JS -->
-
-    <script src="{{ asset('assets/frontend/js/waypoints.min.js') }}"></script><!-- WAYPOINTS JS -->
-    <script src="{{ asset('assets/frontend/js/counterup.min.js') }}"></script><!-- COUNTERUP JS -->
-    <script src="{{ asset('assets/frontend/jjs/waypoints-sticky.min.js') }}"></script><!-- COUNTERUP JS -->
-
-    <script src="{{ asset('assets/frontend/js/isotope.pkgd.min.js') }}"></script><!-- MASONRY  -->
-
-    <script src="{{ asset('assets/frontend/js/owl.carousel.min.js') }}"></script><!-- OWL  SLIDER  -->
-
-
-    <script src="{{ asset('assets/frontend/js/stellar.min.js') }}"></script><!-- PARALLAX BG IMAGE   -->
-    <script src="{{ asset('assets/frontend/js/scrolla.min.js') }}"></script><!-- ON SCROLL CONTENT ANIMTE   -->
-
-    <script src="{{ asset('assets/frontend/js/custom.js') }}"></script><!-- CUSTOM FUCTIONS  -->
-    <script src="{{ asset('assets/frontend/js/shortcode.js') }}"></script><!-- SHORTCODE FUCTIONS  -->
-    <script src="js/switcher.js"></script><!-- SWITCHER FUCTIONS  -->
-    <script src="{{ asset('assets/frontend/js/jquery.bgscroll.js') }}"></script><!-- BACKGROUND SCROLL -->
-    <script src="{{ asset('assets/frontend/js/tickerNews.min.js') }}"></script><!-- TICKERNEWS-->
+    @include('frontend.layouts.script')
 
     <!-- Wrapper Ends -->
 
