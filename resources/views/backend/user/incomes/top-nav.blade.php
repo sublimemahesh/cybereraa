1c862d3b0afaf20ba13cd03b2f179bc6584c6977
Lochana.synotec <lochana.synotec@gmail.com>
2023-06-29 01:59:37 +0530

    #272 fix : redesign tab user panel in backend

--- resources/views/backend/user/incomes/top-nav.blade.php
+++ resources/views/backend/user/incomes/top-nav.blade.php
@@ -1,10 +1,8 @@
 <div class="col-12">
-    <nav>
-        <div class="order nav nav-tabs">
-            <a class="nav-link {{ request()->routeIs('user.incomes.commission') ? 'active' : '' }}" href="{{ !request()->routeIs('user.incomes.commission') ? route('user.incomes.commission') : 'javascript:void(0)' }}">My Commissions</a>
-            <a class="nav-link {{ request()->routeIs('user.incomes.rewards') ? 'active' : '' }}" href="{{ !request()->routeIs('user.incomes.rewards') ? route('user.incomes.rewards') : 'javascript:void(0)' }}">My Rewards</a>
-            <a class="nav-link {{ request()->routeIs('user.earnings.index') ? 'active' : '' }}" href="{{ !request()->routeIs('user.earnings.index') ? route('user.earnings.index') : 'javascript:void(0)' }}">My Earnings</a>
-            <a class="nav-link {{ request()->routeIs('user.earnings.yearly-income-chart') ? 'active' : '' }}" href="{{ !request()->routeIs('user.earnings.yearly-income-chart') ? route('user.earnings.yearly-income-chart') : 'javascript:void(0)' }}">Income Chart</a>
-        </div>
-    </nav>
+    <div class="order nav nav-tabs">
+        <a class="nav-link {{ request()->routeIs('user.incomes.commission') ? 'active' : '' }}" href="{{ !request()->routeIs('user.incomes.commission') ? route('user.incomes.commission') : 'javascript:void(0)' }}">My Commissions</a>
+        <a class="nav-link {{ request()->routeIs('user.incomes.rewards') ? 'active' : '' }}" href="{{ !request()->routeIs('user.incomes.rewards') ? route('user.incomes.rewards') : 'javascript:void(0)' }}">My Rewards</a>
+        <a class="nav-link {{ request()->routeIs('user.earnings.index') ? 'active' : '' }}" href="{{ !request()->routeIs('user.earnings.index') ? route('user.earnings.index') : 'javascript:void(0)' }}">My Earnings</a>
+        <a class="nav-link {{ request()->routeIs('user.earnings.yearly-income-chart') ? 'active' : '' }}" href="{{ !request()->routeIs('user.earnings.yearly-income-chart') ? route('user.earnings.yearly-income-chart') : 'javascript:void(0)' }}">Income Chart</a>
+    </div>
 </div>
--- resources/views/backend/user/teams/top-nav.blade.php
+++ resources/views/backend/user/teams/top-nav.blade.php
@@ -1,11 +1,9 @@
-<div class="col-12">
-    <nav>
-        <div class="order nav nav-tabs">
-            <a class="nav-link {{ request()->routeIs('user.genealogy') ? 'active' : '' }}" href="{{ !request()->routeIs('user.genealogy') ? route('user.genealogy') : 'javascript:void(0)' }}">Genealogy</a>
-            <a class="nav-link {{--{{ request()->routeIs('user.earnings.team-income') ? 'active' : ''  }}--}}" href="{{--{{ !request()->routeIs('user.earnings.team-income') ? route('user.earnings.team-income') : 'javascript:void(0)' }}--}}">Team List</a>
-            <a class="nav-link {{ request()->routeIs('user.earnings.team-income') ? 'active' : '' }}" href="{{ !request()->routeIs('user.earnings.team-income') ? route('user.earnings.team-income') : 'javascript:void(0)' }}">Highest Earnings</a>
-            <a class="nav-link {{ request()->routeIs('user.team.incomes.commission') ? 'active' : '' }}" href="{{ !request()->routeIs('user.team.incomes.commission') ? route('user.team.incomes.commission') : 'javascript:void(0)' }}">Team Incomes</a>
-            <a class="nav-link {{ request()->routeIs('user.earnings.yearly-income-chart') ? 'active' : '' }}" href="{{ !request()->routeIs('user.earnings.yearly-income-chart') ? route('user.earnings.yearly-income-chart', ['team' => true]).'#team-income-chart' : 'javascript:void(0)' }}">Income Chart</a>
-        </div>
-    </nav>
+<div class="col-12"> 
+    <div class="order nav nav-tabs">
+        <a class="nav-link {{ request()->routeIs('user.genealogy') ? 'active' : '' }}" href="{{ !request()->routeIs('user.genealogy') ? route('user.genealogy') : 'javascript:void(0)' }}">Genealogy</a>
+        <a class="nav-link {{--{{ request()->routeIs('user.earnings.team-income') ? 'active' : ''  }}--}}" href="{{--{{ !request()->routeIs('user.earnings.team-income') ? route('user.earnings.team-income') : 'javascript:void(0)' }}--}}">Team List</a>
+        <a class="nav-link {{ request()->routeIs('user.earnings.team-income') ? 'active' : '' }}" href="{{ !request()->routeIs('user.earnings.team-income') ? route('user.earnings.team-income') : 'javascript:void(0)' }}">Highest Earnings</a>
+        <a class="nav-link {{ request()->routeIs('user.team.incomes.commission') ? 'active' : '' }}" href="{{ !request()->routeIs('user.team.incomes.commission') ? route('user.team.incomes.commission') : 'javascript:void(0)' }}">Team Incomes</a>
+        <a class="nav-link {{ request()->routeIs('user.earnings.yearly-income-chart') ? 'active' : '' }}" href="{{ !request()->routeIs('user.earnings.yearly-income-chart') ? route('user.earnings.yearly-income-chart', ['team' => true]).'#team-income-chart' : 'javascript:void(0)' }}">Income Chart</a>
+    </div> 
 </div>
