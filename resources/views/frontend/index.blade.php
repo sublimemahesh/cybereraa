12b8455254a97204078ce9bc1bc217e9141c07b5
Lochana.synotec <lochana.synotec@gmail.com>
2023-06-29 15:47:19 +0530

    #278 Update : cyrrency slider index page in frontend

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -22,102 +22,35 @@
                             <div class="ti_news">
                                 <a href="#">
                                     <img src="{{asset('assets/frontend/images/coin-icon/bitcoin.png') }}" alt="">
-                                    <span>BTC: </span><span>$ 10,633.1</span><span class="text-yellow p-lr5">0.97 %</span></a>
+                                    <span>BTC: </span><span class="bitcoin"></span><span class="text-white p-lr5 bitcoin-change"></span></a>
                             </div>
-                            <div class="ti_news">
-                                <a href="#">
-                                    <img src="{{asset('assets/frontend/images/coin-icon/bitcoin.png') }}" alt="">
-                                    <span>BTC: </span><span>¥ 68,008.1</span><span class="text-danger p-lr5">0.00 %</span></a>
-                            </div>
-                            <div class="ti_news">
-                                <a href="#">
-                                    <img src="{{asset('assets/frontend/images/coin-icon/bitcoin.png') }}" alt="">
-                                    <span>BTC: </span><span>€ 8,699.23</span><span class="text-white p-lr5">1.08 %</span></a>
-                            </div>
+
 
                             <div class="ti_news">
                                 <a href="#">
                                     <img src="{{asset('assets/frontend/images/coin-icon/Ethereum.png') }}" alt="">
-                                    <span>ETH: </span><span>Ƀ 0.08160</span><span class="text-green p-lr5">-0.28 %</span></a>
-                            </div>
-                            <div class="ti_news">
-                                <a href="#">
-                                    <img src="{{asset('assets/frontend/images/coin-icon/Ethereum.png') }}" alt="">
-                                    <span>ETH: </span><span>$ 867.93</span><span class="text-danger p-lr5">-0.60 %</span></a>
-                            </div>
-                            <div class="ti_news">
-                                <a href="#">
-                                    <img src="{{asset('assets/frontend/images/coin-icon/Ethereum.png') }}" alt="">
-                                    <span>ETH: </span><span>¥ 5,549.46</span><span class="text-white p-lr5">-0.28 %</span></a>
-                            </div>
-                            <div class="ti_news">
-                                <a href="#">
-                                    <img src="{{asset('assets/frontend/images/coin-icon/Ethereum.png') }}" alt="">
-                                    <span>ETH: </span><span>€ 709.94</span><span class="text-gray p-lr5">0.26 %</span></a>
+                                    <span>ETH: </span><span class="ethereum"></span><span class="text-white p-lr5 ethereum-change"></span></a>
                             </div>
 
-                            <div class="ti_news">
-                                <a href="#">
-                                    <img src="{{asset('assets/frontend/images/coin-icon/monero.png') }}" alt="">
-                                    <span>XMR: </span><span>Ƀ 0.0276</span><span class="text-green p-lr5">1.25 %</span></a>
-                            </div>
-                            <div class="ti_news">
-                                <a href="#">
-                                    <img src="{{asset('assets/frontend/images/coin-icon/monero.png') }}" alt="">
-                                    <span>XMR: </span><span>$ 295.33</span><span class="text-light-blue p-lr5">0.89 %</span></a>
-                            </div>
-                            <div class="ti_news">
-                                <a href="#">
-                                    <img src="{{asset('assets/frontend/images/coin-icon/monero.png') }}" alt="">
-                                    <span>XMR: </span><span>¥ 1,883.14</span><span class="text-green p-lr5">0.25 %</span></a>
-                            </div>
-                            <div class="ti_news">
-                                <a href="#">
-                                    <img src="{{asset('assets/frontend/images/coin-icon/monero.png') }}" alt="">
-                                    <span>XMR: </span><span>€ 240.56</span><span class="text-red p-lr5">-0.40 %</span></a>
-                            </div>
+
 
                             <div class="ti_news">
                                 <a href="#">
                                     <img src="{{asset('assets/frontend/images/coin-icon/litecoin.png') }}" alt="">
-                                    <span>LTC: </span><span>Ƀ 0.01956</span><span class="text-danger p-lr5">-0.20 %</span></a>
+                                    <span>LTC: </span><span class="litecoin"></span><span class="text-white p-lr5 litecoin-change"></span></a>
                             </div>
-                            <div class="ti_news">
-                                <a href="#">
-                                    <img src="{{asset('assets/frontend/images/coin-icon/litecoin.png') }}" alt="">
-                                    <span>LTC: </span><span>$ 208.06</span><span class="text-green p-lr5">-1.97 %</span></a>
-                            </div>
-                            <div class="ti_news">
-                                <a href="#">
-                                    <img src="{{asset('assets/frontend/images/coin-icon/litecoin.png') }}" alt="">
-                                    <span>LTC: </span><span>¥ 1,330.24</span><span class="text-white p-lr5">-0.20 %</span></a>
-                            </div>
-                            <div class="ti_news">
-                                <a href="#">
-                                    <img src="{{asset('assets/frontend/images/coin-icon/litecoin.png') }}" alt="">
-                                    <span>LTC: </span><span>€ 169.91</span><span class="text-yellow p-lr5">-1.29 %</span></a>
-                            </div>
+
+
+
 
                             <div class="ti_news">
                                 <a href="#">
-                                    <img src="{{asset('assets/frontend/images/coin-icon/DigitalCash.png') }}" alt="">
-                                    <span>DASH: </span><span>Ƀ 0.05590</span><span class="text-white p-lr5">0.26 %</span></a>
+                                    <img src="{{asset('assets/frontend/images/coin-icon/tether.png') }}" alt="">
+                                    <span>TETH: </span><span class="tether">Ƀ 0.05590</span><span class="text-white p-lr5 tether-change"></span></a>
                             </div>
-                            <div class="ti_news">
-                                <a href="#">
-                                    <img src="{{asset('assets/frontend/images/coin-icon/DigitalCash.png') }}" alt="">
-                                    <span>DASH: </span><span>$ 594.64</span><span class="text-green p-lr5">0.37 %</span></a>
-                            </div>
-                            <div class="ti_news">
-                                <a href="#">
-                                    <img src="{{asset('assets/frontend/images/coin-icon/DigitalCash.png') }}" alt="">
-                                    <span>DASH: </span><span>¥ 3,801.65</span><span class="text-red p-lr5">0.99 %</span></a>
-                            </div>
-                            <div class="ti_news">
-                                <a href="#">
-                                    <img src="{{asset('assets/frontend/images/coin-icon/DigitalCash.png') }}" alt="">
-                                    <span>DASH: </span><span>€ 486.29</span><span class="text-yellow p-lr5">-10.18 %</span></a>
-                            </div>
+
+
+
                         </div>
                     </div>
                 </div>
@@ -416,6 +349,7 @@
     <script src="{{asset('assets/frontend/plugins/revolution/revolution/js/extensions/revolution-plugin.js') }}"></script>
     <!-- REVOLUTION SLIDER FUNCTION  ===== -->
     <script src="{{ asset('assets/frontend/js/rev-script-1.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/coin_prices.js') }}"></script>
 
     @endsection
 
