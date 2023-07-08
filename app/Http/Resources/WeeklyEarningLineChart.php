d6369983c4860c042e5d1574b6638b644ded201d
hansajith18 <hansajith18@gmail.com>
2023-07-08 08:55:10 +0530

    Fix: Set Wallet page line chart empty earnings to 0

--- app/Http/Resources/WeeklyEarningLineChart.php
+++ app/Http/Resources/WeeklyEarningLineChart.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class WeeklyEarningLineChart extends JsonResource
@@ -9,6 +10,27 @@ class WeeklyEarningLineChart extends JsonResource
 
     public function toArray($request)
     {
+        $today = Carbon::now()->shortEnglishDayOfWeek;
+        $data = [];
+        switch ($today) {
+            case 'Sat':
+                $data['Sat'] = 0;
+            case 'Fri':
+                $data['Fri'] = 0;
+            case 'Thu':
+                $data['Thu'] = 0;
+            case 'Wed':
+                $data['Wed'] = 0;
+            case 'Tue':
+                $data['Tue'] = 0;
+            case 'Mon':
+                $data['Mon'] = 0;
+            case 'Sun':
+                $data['Sun'] = 0;
+                break;
+            default:
+                break;
+        }
         $line_chart_data = [
             //'labels' => ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'], // DAYNAME(created_at)
             'labels' => ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'], // DAYOFWEEK(created_at)
@@ -16,31 +38,35 @@ class WeeklyEarningLineChart extends JsonResource
         ];
         $packageData = [
             'label' => 'PACKAGE',
-            'data' => [],
-            //'borderColor' => 'rgb(255, 99, 132)',//red
+            'data' => $data,
+            'borderWidth' => 1,
+            'borderColor' => 'rgb(255, 99, 132,0.2)',//red
             'backgroundColor' => 'rgb(255, 99, 132,0.5)',
-            'pointBackgroundColor' => 'rgb(255, 99, 132,0.5)',
+            'pointBackgroundColor' => 'rgb(255, 99, 132)',
         ];
         $directData = [
             'label' => 'DIRECT',
-            'data' => [],
-            //'borderColor' => 'rgb(54, 162, 235)',//blue
+            'data' => $data,
+            'borderWidth' => 1,
+            'borderColor' => 'rgb(54, 162, 235,0.2)',//blue
             'backgroundColor' => 'rgb(54, 162, 235,0.5)',
-            'pointBackgroundColor' => 'rgb(54, 162, 235,0.5)',
+            'pointBackgroundColor' => 'rgb(54, 162, 235)',
         ];
         $indirectData = [
             'label' => 'INDIRECT',
-            'data' => [],
-            //'borderColor' => 'rgb(75, 192, 192)',//green
+            'data' => $data,
+            'borderWidth' => 1,
+            'borderColor' => 'rgb(75, 192, 192,0.2)',//green
             'backgroundColor' => 'rgb(75, 192, 192,0.5)',
-            'pointBackgroundColor' => 'rgb(75, 192, 192,0.5)',
+            'pointBackgroundColor' => 'rgb(75, 192, 192)',
         ];
         $p2pData = [
             'label' => 'P2P',
-            'data' => [],
-            //'borderColor' => 'rgb(255, 205, 86)', //yellow
+            'data' => $data,
+            'borderWidth' => 1,
+            'borderColor' => 'rgb(255, 205, 86,0.2)', //yellow
             'backgroundColor' => 'rgb(255, 205, 86,0.5)',
-            'pointBackgroundColor' => 'rgb(255, 205, 86,0.5)',
+            'pointBackgroundColor' => 'rgb(255, 205, 86)',
         ];
         foreach ($this->resource as $earning) {
             if ($earning->type === 'PACKAGE') {
