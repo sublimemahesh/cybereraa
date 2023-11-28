05f48d20f37df009b115d00620f0c3e7dcd5cf2f
Lochana.synotec <lochana.synotec@gmail.com>
2023-11-28 19:47:03 +0530

    #11 Dashboard header redesign user panel - back-end

--- resources/views/backend/layouts/header.blade.php
+++ resources/views/backend/layouts/header.blade.php
@@ -7,7 +7,7 @@
         <img src="{{ asset('assets/backend/images/logo/logo-text-color.png') }}" class="brand-title color-title" alt="">
     </a>
     <h3 class="nav-username">@yield('header-title2')</h3>
-    <div class="nav-control">
+    {{-- <div class="nav-control">
         <div class="hamburger">
             <span class="line"></span><span class="line"></span><span class="line"></span>
             <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -22,38 +22,10 @@
                 <rect y="22" height="4" rx="2" fill="#2A353A" width="400"></rect>
             </svg>
         </div>
-    </div>
+    </div> --}}
 </div>
 <!--********************************** Nav header end ***********************************-->
 
 <!--********************************** Header start ***********************************-->
-<div class="header">
-    <div class="header-content">
-        <nav class="navbar navbar-expand">
-            <div class="collapse navbar-collapse justify-content-between">
-                <div class="header-left">
-                    <div class="dashboard_bar ">
-                        {{-- <h3 class="h3-txt">@yield('header-title')</h3> --}}
-                        <span class="txte-right">
-                            {{-- @yield('header-title2') --}}
-                        </span>
-                    </div>
-                </div>
 
-                {{-- <div class="navbar-nav header-right">
-                    <div class="dz-side-menu">
-                        <ul>
-                            <li class="nav-item dropdown header-profile">
-                                <a class="nav-link" href="javascript:void(0);" role="button" data-bs-toggle="dropdown">
-                                    <img src="{{ Auth::user()->profile_photo_url }}" alt="">
-                                </a>
-
-                            </li>
-                        </ul>
-                    </div>
-                </div> --}}
-            </div>
-        </nav>
-    </div>
-</div>
 <!--********************************** Header end ti-comment-alt ***********************************-->
