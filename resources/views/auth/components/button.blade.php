f87a835b2e4fca05e502f90aaca1cc1f8bab99e9
Lochana.synotec <lochana.synotec@gmail.com>
2023-12-19 10:57:59 +0530

    #137 login ,register , forgot-password  redesign  UI Design change - User Panel Backend #137

--- resources/views/auth/components/button.blade.php
+++ resources/views/auth/components/button.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center justify-end mt-4">
 
-    <button {{ $attribute }} type='submit' class='ml-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition  btn-block btn btn-primary btn-block btn btn-primary btn-block' data-devil='bgc:#330438'>
+    <button {{ $attribute }} type='submit' class='ml-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition  btn btn-primary btn-block btn btn-primary btn-block btn btn-primary btn-block'>
         {{ $slot }}
     </button><br>
 
