f8edd9fc0fcaecba3052b822b2e11678d426d1e3
Lochana.synotec <lochana.synotec@gmail.com>
2023-12-29 12:58:16 +0530

    #182  Approve Manual Transaction design change admin backend - UI design

--- resources/views/backend/admin/users/transactions/approve-manual.blade.php
+++ resources/views/backend/admin/users/transactions/approve-manual.blade.php
@@ -2,15 +2,15 @@
     @section('title', 'Approve Manual Transaction | Payments')
     @section('header-title', 'Approve Manual Transaction')
     @section('plugin-styles')
-        <!-- Datatable -->
+    <!-- Datatable -->
     @endsection
 
     @section('breadcrumb-items')
-        <li class="breadcrumb-item">Approve Transaction</li>
+    <li class="breadcrumb-item">Approve Transaction</li>
     @endsection
 
     <div class="row">
-        <div class="col-sm-8">
+        <div class="col-sm-12">
             <div class="card">
                 <div class="card-body">
                     <form class="theme-form" enctype="multipart/form-data" id="approval-form">
@@ -19,60 +19,90 @@
                             <hr>
                         </div>
                         <div class="row">
-                            <div class="col-sm-6">
-                                <div class="mb-2">
-                                    <p><b>Payment id:</b> {{ $transaction->id }}</p>
-                                    <p><b>User:</b> {{ $transaction->user_id }} - {{ $transaction->user->username }}</p>
-                                    <p><b>Purchased By:</b> {{ $transaction->purchaser_id }} - {{ $transaction->purchaser->username }}</p>
-                                    <p><b>Package:</b> {{ $transaction->create_order_request_info->goods->goodsName ?? '-' }}</p>
-                                    <p><b>Currency:</b> {{ $transaction->currency }}</p>
-                                    <p><b>Amount:</b> {{ $transaction->amount }}</p>
-                                    <p><b>Gas Fee: </b> {{ $transaction->gas_fee }}</p>
-                                    <p><b>Pay Method:</b> {{ $transaction->type }}/{{ $transaction->pay_method }}</p>
-                                    @if($transaction->status === 'REJECTED')
-                                        <p><b>Repudiate note:</b> {{ $transaction->repudiate_note }}</p>
-                                    @endif
-                                    <p><b>Status:</b> {{ $transaction->status }}</p>
-                                </div>
-                            </div>
-                            <div class="col-sm-6 m-auto text-center">
-                                <img src="{{ storage('user/manual-purchase/' . $transaction->proof_document) }}" alt="" class="img-thumbnail mw-100">
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <hr>
-                                <div class="mb-2 mt-2">
-                                    <div class="text-info">
-                                        <label for="proof_document">Proof:</label>
-                                        <a href="{{ asset('storage/user/manual-purchase/' . $transaction->proof_document) }}" target="_blank">View Proof</a>
+                            <div class="col-sm-8">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="mb-2">
+                                            <p data-devil="fs:15"><b>Payment id:</b> {{ $transaction->id }}</p>
+                                            <p data-devil="fs:15"><b>User:</b> {{ $transaction->user_id }} - {{ $transaction->user->username }}</p>
+                                            <p data-devil="fs:15"><b>Purchased By:</b> {{ $transaction->purchaser_id }} - {{
+                                                $transaction->purchaser->username }}</p>
+                                            <p data-devil="fs:15"><b>Package:</b> {{ $transaction->create_order_request_info->goods->goodsName ??
+                                                '-' }}</p>
+                                            <p data-devil="fs:15"><b>Currency:</b> {{ $transaction->currency }}</p>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="mb-3 mt-1">
-                                    <div class="text-info">
-                                        <label for="proof_document">Transaction ID:</label>
-                                        <a href="javascript:void(0)">{{ $transaction->transaction_id }}</a>
-                                    </div>
-                                </div>
-                                <hr>
-                                <p>
-                                    Please confirm access to your account by entering the <code>password</code> and
-                                    <code>authentication code</code> provided by your authenticator application
-                                </p>
+                                    <div class="col-sm-6">
+                                        <div class="mb-2">
 
-                                <div class="mb-3 mt-2">
-                                    <label for="password">Password</label>
-                                    <input id="password" type="password" name="password" data-input='payout' class="form-control" autocomplete="new-password">
-                                </div>
-                                @if(Auth::user()?->two_factor_secret && in_array( \Laravel\Fortify\TwoFactorAuthenticatable::class, class_uses_recursive(Auth::user()),true))
-                                    <div class="mb-3 mt-2">
-                                        <label for="code">Two Factor code / Recovery Code</label>
-                                        <input id="code" name="code" type="password" data-input='payout' class="form-control" autocomplete="one-time-password" placeholder="2FA code OR Recovery Code">
+                                            <p data-devil="fs:15"><b>Amount:</b> {{ $transaction->amount }}USDT</p>
+                                            <p data-devil="fs:15"><b>Gas Fee: </b> {{ $transaction->gas_fee }}USDT</p>
+                                            <p data-devil="fs:15"><b>Pay Method:</b> {{ $transaction->type }}/{{ $transaction->pay_method }}</p>
+                                            @if($transaction->status === 'REJECTED')
+                                            <p data-devil="fs:15"><b>Repudiate note:</b> {{ $transaction->repudiate_note }}</p>
+                                            @endif
+                                            <p data-devil="fs:15"><b>Status:</b> {{ $transaction->status }}</p>
+                                        </div>
                                     </div>
-                                @endif
+                                </div>
+
+                                <div>
+                                    <div class="fs-30 text-center font-w600" data-devil="c:#fff mb:15">Total Amount: {{
+                                        $transaction->amount + $transaction->gas_fee }}USDT</div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <hr>
+                                        <div class="mb-2 mt-2">
+                                            <div class="text-info">
+                                                <label for="proof_document">Proof:</label>
+                                                <a href="{{ asset('storage/user/manual-purchase/' . $transaction->proof_document) }}"
+                                                    target="_blank">View Proof</a>
+                                            </div>
+                                        </div>
+                                        <div class="mb-3 mt-1">
+                                            <div class="text-info">
+                                                <label for="proof_document">Transaction ID:</label>
+                                                <a href="javascript:void(0)" class="copy-to-clipboard"
+                                                    data-clipboard-text="{{ $transaction->transaction_id }}">{{
+                                                    $transaction->transaction_id }} <i class="fa fa-clone"
+                                                        style="font-size: 17px;" data-devil="ml:5"></i></a>
+                                            </div>
+                                        </div>
+                                        <hr>
+                                        <p>
+                                            Please confirm access to your account by entering the <code>password</code>
+                                            and
+                                            <code>authentication code</code> provided by your authenticator application
+                                        </p>
+
+                                        <div class="mb-3 mt-2">
+                                            <label for="password">Password</label>
+                                            <input id="password" type="password" name="password" data-input='payout'
+                                                class="form-control" autocomplete="new-password">
+                                        </div>
+                                        @if(Auth::user()?->two_factor_secret && in_array(
+                                        \Laravel\Fortify\TwoFactorAuthenticatable::class,
+                                        class_uses_recursive(Auth::user()),true))
+                                        <div class="mb-3 mt-2">
+                                            <label for="code">Two Factor code / Recovery Code</label>
+                                            <input id="code" name="code" type="password" data-input='payout'
+                                                class="form-control" autocomplete="one-time-password"
+                                                placeholder="2FA code OR Recovery Code">
+                                        </div>
+                                        @endif
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-success" id="approveTrx">APPROVE</button>
+                            </div>
+
+
+                            <div class="col-sm-4 m-auto text-center">
+                                <img src="{{ storage('user/manual-purchase/' . $transaction->proof_document) }}" alt=""
+                                    class="img-thumbnail mw-100" data-devil="mt:-92" data-dxs="mt:15">
                             </div>
                         </div>
-                        <button type="submit" class="btn btn-success" id="approveTrx">APPROVE</button>
                     </form>
                 </div>
             </div>
@@ -80,6 +110,20 @@
     </div>
 
     @push('scripts')
-        <script src="{{ asset('assets/backend/js/admin/transactions/manual-trx.js') }}"></script>
+    <script src="{{ asset('assets/backend/js/admin/transactions/manual-trx.js') }}"></script>
+
+    <script src="{{ asset('assets/backend/vendor/clipboard/clipboard.min.js') }}"></script>
+    <script !src="">
+        let clipboard = new ClipboardJS('.copy-to-clipboard');
+
+            // Handle copy success
+            clipboard.on('success', function (e) {
+                Toast.fire({
+                    icon: 'success', title: 'Address copied to clipboard!',
+                })
+                e.clearSelection();
+            });
+    </script>
+
     @endpush
 </x-backend.layouts.app>
