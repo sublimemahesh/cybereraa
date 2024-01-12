62963a175efba8e390d4ecceab68c7260c7aeee5
Lochana.synotec <lochana.synotec@gmail.com>
2024-01-12 09:05:08 +0530

    #222  Select Payment method  dispay : none  Purchase For txt input  User panel Backend - UI Design

--- resources/views/backend/user/packages/custom-amount-deposit.blade.php
+++ resources/views/backend/user/packages/custom-amount-deposit.blade.php
@@ -229,7 +229,7 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-sm-12 mb-4">
+                            <div class="col-sm-12 mb-4" data-devil="dis:none">
                                 <div class="mb-3 mt-2">
                                     <label for="purchase_for">Purchase For</label>
                                     <select class="single-select-placeholder js-states select2-hidden-accessible"
