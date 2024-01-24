0206a85496d077f2ebfcc2be132a4b2445b3f69d
hansajith18 <hansajith18@gmail.com>
2024-01-24 14:45:06 +0530

    Disable Maintenance Mode

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::group(['middleware' => 'maintenance_mode'], function () {
-    Route::get('/login', 'FrontendController@index');
-    Route::get('/forgot-password', 'FrontendController@index');
+//    Route::get('/login', 'FrontendController@index');
+//    Route::get('/forgot-password', 'FrontendController@index');
 
     Route::get('/', 'FrontendController@index')->name('/');
     Route::get('about-us', 'FrontendController@about')->name('about');
